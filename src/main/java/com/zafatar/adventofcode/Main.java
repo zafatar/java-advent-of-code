6fd2948cd63ddde6c4e4524122c8c18603986730
zafatar <zaferkiziltoprak@gmail.com>
2018-01-04 18:25:53 +0100

    Solutions for Day4, Day5 and Day6 of 2017 added

--- src/main/java/com/zafatar/adventofcode/Main.java
+++ src/main/java/com/zafatar/adventofcode/Main.java
@@ -3,6 +3,9 @@ package com.zafatar.adventofcode;
 import com.zafatar.adventofcode.y2017.Day1;
 import com.zafatar.adventofcode.y2017.Day2;
 import com.zafatar.adventofcode.y2017.Day3;
+import com.zafatar.adventofcode.y2017.Day4;
+import com.zafatar.adventofcode.y2017.Day5;
+import com.zafatar.adventofcode.y2017.Day6;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -13,17 +16,35 @@ public class Main {
 		day1.solvePart1();				// Day #1 - Part #1
 		day1.solvePart2();				// Day #2 - Part #2
 		System.out.println(separator);
-		
+
 		// Year 2017 - Day #2.
 		Day2 day2 = new Day2();			
 		day2.solvePart1();				// Day #2 - Part #1
 		day2.solvePart2();				// Day #2 - Part #2
 		System.out.println(separator);
-		
+
 		// Year 2017 - Day #3.
 		Day3 day3 = new Day3();			
 		day3.solvePart1();				// Day #3 - Part #1
 		day3.solvePart2();				// Day #3 - Part #2		
+		System.out.println(separator);
+
+		// Year 2017 - Day #4.
+		Day4 day4 = new Day4();			
+		day4.solvePart1();				// Day #4 - Part #1
+		day4.solvePart2();				// Day #4 - Part #2		
+		System.out.println(separator);
+
+		// Year 2017 - Day #5.
+		Day5 day5 = new Day5();			
+		day5.solvePart1();				// Day #5 - Part #1
+		day5.solvePart2();				// Day #5 - Part #2		
+		System.out.println(separator);
+		
+		// Year 2017 - Day #6.
+		Day6 day6 = new Day6();			
+		day6.solvePart1();				// Day #6 - Part #1
+		day6.solvePart2();				// Day #6 - Part #2		
 		System.out.println(separator);
 	}
 }
---
+++ src/main/java/com/zafatar/adventofcode/y2017/Day4.java
@@ -0,0 +1,149 @@
+package com.zafatar.adventofcode.y2017;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import com.zafatar.adventofcode.utils.FileReader;
+
+public class Day4 implements Day {
+	public ArrayList<ArrayList<String>> input = new ArrayList<ArrayList<String>>();
+	public int totalPhrases = 0;
+	public int notValid = 0;
+	public int valid = 0;
+
+	public Day4() {
+		super();
+		this.prepareInput("/2017/Day4/input");
+	}
+
+	@Override
+	public void solvePart1() {
+		// reset the counters.
+		this.totalPhrases = 0;
+		this.notValid = 0;
+
+		for (ArrayList<String> phrase : this.input) {
+			HashMap<String, Integer> seen = new HashMap<String, Integer>();
+			for (String word : phrase) {
+				if (seen.get(word) != null) {
+					this.notValid++;
+					break;
+				} else
+					seen.put(word, 1);
+			}
+
+			this.totalPhrases++;
+		}
+
+		int valid = this.totalPhrases - this.notValid;
+		System.out.println("Day #4 - Part #1: " + valid + " / " + this.totalPhrases);
+	}
+
+	@Override
+	public void solvePart2() {
+		// reset the counters.
+		this.totalPhrases = 0;
+		this.notValid = 0;
+
+		for (ArrayList<String> phrase : this.input) {
+			HashMap<String, Integer> seen = new HashMap<String, Integer>();
+			for (String word : phrase) {
+				// This time, use canonical form of the string to detect
+				// uniqueness in the array.
+				String canonical = this.generateCanonical(word);
+				if (seen.get(canonical) != null) {
+					this.notValid++;
+					break;
+				} else
+					seen.put(canonical, 1);
+			}
+
+			this.totalPhrases++;
+		}
+
+		int valid = this.totalPhrases - this.notValid;
+		System.out.println("Day #4 - Part #2: " + valid + " / " + this.totalPhrases);
+	}
+
+	/**
+	 * This method creates a comparable unique signature for each string.
+	 * Basically, it orders the chars in the string alphabetically.
+	 * 
+	 * @param word the input string
+	 * @return the ordered string of the input.
+	 */
+	private String generateCanonical(String word) {
+		// Convert the string into char array.
+		char[] charArray = word.toCharArray();
+
+		// Sort the char array to create same unique signature
+		// with the other possible words.
+		Arrays.sort(charArray);
+		String sortedString = new String(charArray);
+
+		return sortedString;
+	}
+
+	@Override
+	public void prepareInput(String filepath) {
+		FileReader fr = new FileReader(filepath);
+
+		String[] lines = fr.getContent().split("\n");
+		for (int i = 0; i < lines.length; i++) {
+			ArrayList<String> row = new ArrayList<String>();
+
+			// TODO: better splitting.
+			String[] phrases = lines[i].split(" ");
+			for (String phrase : phrases) {
+				row.add(phrase);
+				// System.out.println(">" + phrase);
+			}
+
+			// System.out.println("\n");
+			this.input.add(row);
+		}
+	}
+}
+
+/**
+ * --- Day 4: High-Entropy Passphrases ---
+ * 
+ * A new system policy has been put in place that requires all accounts to use a
+ * passphrase instead of simply a password. A passphrase consists of a series of
+ * words (lowercase letters) separated by spaces.
+ * 
+ * To ensure security, a valid passphrase must contain no duplicate words.
+ * 
+ * For example:
+ * 
+ * - aa bb cc dd ee is valid. 
+ * - aa bb cc dd aa is not valid - the word aa appears more than once. 
+ * - aa bb cc dd aaa is valid - aa and aaa count as different words.
+ * 
+ * The system's full passphrase list is available as your puzzle input. How many
+ * passphrases are valid?
+ * 
+ * Your puzzle answer was ***. --- Part Two ---
+ * 
+ * For added security, yet another system policy has been put in place. Now, a
+ * valid passphrase must contain no two words that are anagrams of each other -
+ * that is, a passphrase is invalid if any word's letters can be rearranged to
+ * form any other word in the passphrase.
+ * 
+ * For example:
+ * 
+ * - abcde fghij is a valid passphrase. 
+ * - abcde xyz ecdab is not valid - the letters from the third word can be 
+ *   rearranged to form the first word. 
+ * - a ab abc abd abf abj is a valid passphrase, because all letters need 
+ *   to be used when forming another word.
+ * - iiii oiii ooii oooi oooo is valid. 
+ * - oiii ioii iioi iiio is not valid - any of these words can be 
+ *   rearranged to form any other word.
+ * 
+ * Under this new system policy, how many passphrases are valid?
+ * 
+ * Your puzzle answer was ***.
+ * 
+ */
